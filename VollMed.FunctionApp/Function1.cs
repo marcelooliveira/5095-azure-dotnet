fa95ee0cd0c748accb524422fa27b7a557885a66
marcelooliveira <mclricardo@gmail.com>
2025-10-19 11:50:33 -0300

    5095-video6.9-Adicionando a lógica da função Azure.mp4

--- VollMed.FunctionApp/Function1.cs
+++ VollMed.FunctionApp/Function1.cs
@@ -1,8 +1,10 @@
-using System;
-using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace VollMed.FunctionApp;
 
@@ -15,18 +17,51 @@ public class Function1
         _logger = logger;
     }
 
-    [Function(nameof(Function1))]
-    public async Task Run(
-        [ServiceBusTrigger("vollmedqueue", Connection = "")]
-        ServiceBusReceivedMessage message,
-        ServiceBusMessageActions messageActions)
+    [Function("ResumoMensalFunction")]
+    [CosmosDBOutput(
+        databaseName: "%AzureCosmosDB_DatabaseName%",
+        containerName: "%AzureCosmosDB_ContainerName%",
+        Connection = "AzureCosmosDB_ConnectionString")]
+    public async Task<object?> Run(
+        [ServiceBusTrigger("vollmedqueue", Connection = "ServiceBusConnection")]
+        string message,
+        ServiceBusMessageActions messageActions,
+        [SqlInput(
+        "SELECT m.Id as MedicoId, m.Nome as MedicoNome, COUNT(c.Id) as QtdeConsultas FROM Medicos m LEFT JOIN Consultas c ON m.Id = c.MedicoId AND YEAR(c.Data) = @Ano AND MONTH(c.Data) = @Mes WHERE m.Id = @MedicoId GROUP BY m.Id, m.Nome",
+        "SqlConnectionString", System.Data.CommandType.Text, "@MedicoId={MedicoId},@Ano={Ano},@Mes={Mes}")]
+        IEnumerable<ConsultaPorMedico> consultas,
+        FunctionContext context)
     {
-        _logger.LogInformation("Message ID: {id}", message.MessageId);
-        _logger.LogInformation("Message Body: {body}", message.Body);
-        _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+        try
+        {
+            _logger.LogInformation("Message: {message}", message);
 
-        // Complete the message
-        await messageActions.CompleteMessageAsync(message);
+            var honorarioPorConsulta = 100m;
+            var consultaMsg = JsonSerializer.Deserialize<ConsultaQueueMessage>(message);
+            _logger.LogInformation($"Processing consulta for MedicoId={consultaMsg.MedicoId}, Ano={consultaMsg.Ano}, Mes={consultaMsg.Mes}");
+
+            if (consultas == null || !consultas.Any())
+                return null;
+
+            var consulta = consultas.SingleOrDefault();
+            var honorarios = (consulta.QtdeConsultas + 1) * honorarioPorConsulta;
+            var resultadoMensal = new ResultadoMensal(
+                id: consulta.MedicoId.ToString("00000") + "-" + consultaMsg.Ano.ToString() + "-" + consultaMsg.Mes.ToString("00"),
+                medicoId: consulta.MedicoId,
+                medicoNome: consulta.MedicoNome,
+                ano: consultaMsg.Ano,
+                mes: consultaMsg.Mes,
+                qtdeConsultas: consulta.QtdeConsultas + 1,
+                honorarios: honorarios
+            );
+
+            return resultadoMensal;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to process consulta");
+            return null;
+        }
     }
 }
 
