6441e8896a50072894f5ac3e7073f1b9fa34c8c0
marcelooliveira <mclricardo@gmail.com>
2025-11-02 12:16:41 -0300

    5095-video8.3-Configurando Redis no projeto Azure Function App

--- VollMed.FunctionApp/GerarProntuarioMedico.cs
+++ VollMed.FunctionApp/GerarProntuarioMedico.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
 
 namespace VollMed.FunctionApp;
 
@@ -20,4 +21,25 @@ public class GerarProntuarioMedico
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
     }
+}
+
+
+public class Prontuario
+{
+    public long MedicoId { get; set; }
+    public string MedicoNome { get; set; } = string.Empty;
+    public string MedicoCrm { get; set; } = string.Empty;
+    public int MedicoEspecialidade { get; set; }
+    public DateTime ConsultaData { get; set; }
+    public string ConsultaPaciente { get; set; } = string.Empty;
+}
+
+public enum Especialidade
+{
+    [Display(Name = "Cardiologia")] Cardiologia = 1,
+    [Display(Name = "Neurocirurgia")] Neurocirurgia = 2,
+    [Display(Name = "Cirurgia Geral")] CirurgiaGeral = 3,
+    [Display(Name = "Pediatria")] Pediatria = 4,
+    [Display(Name = "Oncologia")] Oncologia = 5,
+    [Display(Name = "Diagnóstico")] Diagnostico = 6
 }
